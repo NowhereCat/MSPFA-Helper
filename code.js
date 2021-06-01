c4c35847d06d03afb47305cc6dfdaeab24fbecba
ThomasTesla766 <tombrony91@gmail.com>
2021-06-01 14:52:43 -0300

    Update code.js

--- code.js
+++ code.js
@@ -1,9 +1,3 @@
-app.use(function(req, res, next){
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 export default class User {
   constructor(name, age){
     this.name = Name
